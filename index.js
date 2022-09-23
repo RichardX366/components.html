aa7ba9325e8c61309f76865dc9abd7990db49f93
Richard X <richardx366@gmail.com>
2022-09-23 18:59:54 -0400

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 const routeComponentsScript = (app) => {
-  app.get(__dirname + '/__components.js', (req, res) => {
-    res.sendFile('public/__components.js');
+  app.get('/__components.js', (req, res) => {
+    res.sendFile(__dirname + '/public/__components.js');
   });
-  app.get(__dirname + '/__components.css', (req, res) => {
-    res.sendFile('public/__components.css');
+  app.get('/__components.css', (req, res) => {
+    res.sendFile(__dirname + '/public/__components.css');
   });
 };
 exports.default = routeComponentsScript;
