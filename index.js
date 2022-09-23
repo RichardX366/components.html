c77e7e557f6a8c8fcf35a5d34edc46cab13d32a1
Richard X <richardx366@gmail.com>
2022-09-23 18:54:43 -0400

    fix

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 Object.defineProperty(exports, '__esModule', { value: true });
-const routeComponentsScript = (app, route) => {
-  app.use(require('express').static('public', route));
+const routeComponentsScript = (app) => {
+  app.get('/__components.js', (req, res) => {
+    res.sendFile(require('path').resolve(__dirname, '../public/components.js'));
+  });
+  app.get('/__components.css', (req, res) => {
+    res.sendFile(
+      require('path').resolve(__dirname, '../public/components.css'),
+    );
+  });
 };
 exports.default = routeComponentsScript;
